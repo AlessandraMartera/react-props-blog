cb0ce2b421a3798f53ef094467099da0137f1bc6
alessandra martera <ale.martera@gmail.com>
2023-11-30 18:09:07 +0100

    update checkPost

--- src/components/TheMain.jsx
+++ src/components/TheMain.jsx
@@ -1,13 +1,23 @@
 import { list } from 'postcss'
-import { posts } from './../db/posts' 
+// import { posts } from './../db/posts' 
 import Card from './Card' 
 
-
+const posts = []
 const colorTags = {
     'html': 'bg-orange-300', //orange
     'css': 'bg-blue-300', // blue
     'js': 'bg-yellow-300', // yellow
     'php': 'bg-indigo-300', // violette
+}
+
+function checkPost(posts){
+    if(posts.length  > 0){
+        console.log(true)
+        return true
+    }else{
+        console.log(false)
+        return false
+    }
 }
 
 function tagsList() {
@@ -25,35 +35,43 @@ function tagsList() {
     return tagsList
 }
 export default function TheMain(){
- 
+
+
     return (
     <>
     <main> 
-        <div>
-            <h1 className='my-6 font-bold text-center text-4xl' >
-                 i tags che troverari i questa pagina sono: 
-            </h1>
-           
-            
-            <ul className='flex justify-center'> 
-                {tagsList().map((tag,i) => ( <li className={colorTags[tag]} key={i}>{tag}</li>))}
-            </ul>
+    
+        <h2 className={checkPost(posts) ? 'hidden' : 'text-center text-8xl'}>
+            Non ci sono post disponibili al momento
+        </h2>
 
-        </div>
+        <div className={checkPost(posts) || 'hidden'}>
+            <div>
+                    <h1 className='my-6 font-bold text-center text-4xl' >
+                        i tags che troverari i questa pagina sono: 
+                    </h1>
+                
+                    
+                    <ul className='flex justify-center'> 
+                        {tagsList().map((tag,i) => ( <li className={colorTags[tag]} key={i}>{tag}</li>))}
+                    </ul>
 
-        <div className='listCard'>
-        { posts.map( (post, i) => {
+                </div>
 
-           return <Card key={i} 
-           title={ post.title}
-           image={ post.image }
-           content={ post.content}
-           tags={ post.tags }
-           published={ post.published }
-           />
-        
-        })}
-           
+                <div className='listCard' >
+                { posts.map( (post, i) => {
+
+                return <Card key={i} 
+                title={ post.title}
+                image={ post.image }
+                content={ post.content}
+                tags={ post.tags }
+                published={ post.published }
+                />
+                
+                })}
+                
+                </div>
         </div>
     </main>
     </>
--- src/db/posts.js
+++ src/db/posts.js
@@ -1,65 +1,65 @@
 export const posts = [
-    {
-      id: 1,
-      title: "Titolo del Post 1",
-      image: "ramen.jpg",
-      content:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.",
-      tags: ["html", "css"],
-      published: true,
-    },
-    {
-      id: 2,
-      title: "campeggio",
-      image: "campeggio.jpg",
-      content:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.",
-      tags: ["js", "css"],
-      published: true,
-    },
-    {
-      id: 3,
-      title: "Titolo del Post 3",
-      image: "",
-      content:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.",
-      tags: ["js", "php"],
-      published: false,
-    },
-    {
-      id: 4,
-      title: "Titolo del Post 4",
-      image: "",
-      content:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.",
-      tags: ["html"],
-      published: false,
-    },
-    {
-      id: 5,
-      title: "Titolo del Post 5",
-      image: "",
-      content:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.",
-      tags: ["html"],
-      published: false,
-    },
-    {
-      id: 6,
-      title: "Titolo del Post 6",
-      image: "fiori.jpg",
-      content:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.",
-      tags: ["html", "css", "php"],
-      published: true,
-    },
-    {
-      id: 7,
-      title: "Titolo del Post 7",
-      image: "",
-      content:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.",
-      tags: ["html"],
-      published: false,
-    },
+  {
+    id: 1,
+    title: "Titolo del Post 1",
+    image: "ramen.jpg",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.",
+    tags: ["html", "css"],
+    published: true,
+  },
+  {
+    id: 2,
+    title: "campeggio",
+    image: "campeggio.jpg",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.",
+    tags: ["js", "css"],
+    published: true,
+  },
+  {
+    id: 3,
+    title: "Titolo del Post 3",
+    image: "",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.",
+    tags: ["js", "php"],
+    published: false,
+  },
+  {
+    id: 4,
+    title: "Titolo del Post 4",
+    image: "",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.",
+    tags: ["html"],
+    published: false,
+  },
+  {
+    id: 5,
+    title: "Titolo del Post 5",
+    image: "",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.",
+    tags: ["html"],
+    published: false,
+  },
+  {
+    id: 6,
+    title: "Titolo del Post 6",
+    image: "fiori.jpg",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.",
+    tags: ["html", "css", "php"],
+    published: true,
+  },
+  {
+    id: 7,
+    title: "Titolo del Post 7",
+    image: "",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.",
+    tags: ["html"],
+    published: false,
+  },
   ];
